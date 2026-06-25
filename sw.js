8f9a5aa4442ddfad13cc0abfb693c611a0929371
yechco-droid <yechco@gmail.com>
2026-06-25 15:28:27 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'asher-yatzar-v3';
+const FILES = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
